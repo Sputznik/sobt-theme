2e180fb1e7ec64f570991e736b116ce379574799
psychome004 <anil.stephen66@gmail.com>
2022-08-31 17:07:51 +0530

    Add interviews template

---
+++ orbit-query/articles-interviews.php
@@ -0,0 +1,7 @@
+<div id="<?php _e( $atts['id'] );?>" class="sobt-grid sobt-grid-3 sobt-interviews" data-target="article.post-card" data-url="<?php _e( $atts['url'] );?>">
+	<?php while( $this->query->have_posts() ) : $this->query->the_post();?>
+    <article class="post-card">
+      <?php get_template_part( 'partials/orbit/post-interview' );?>
+    </article>
+	<?php endwhile;?>
+</div>
---
+++ partials/orbit/post-interview.php
@@ -0,0 +1,12 @@
+<?php
+	$thumbnail = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full' )[0];
+	$permalink = get_the_permalink();
+?>
+<div class="sobt-thumbnail-bg" style="background-image: url( <?php _e( $thumbnail );?> );">
+  <a href="<?php _e( $permalink );?>" class="img-link">
+    <div class="post-desc">
+    	<h4 class="title"><?php the_title();?></h4>
+      <?php echo do_shortcode("[sobt_coauthors]"); ?>
+    </div>
+  </a>
+</div>
