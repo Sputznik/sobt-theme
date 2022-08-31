f64bfd99bf5106d5ccae5431dfd8e339af3e1d74
psychome004 <anil.stephen66@gmail.com>
2022-08-31 17:09:41 +0530

    Add category template

---
+++ category.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * The template for displaying category page
+ */
+get_header();
+
+$category = get_queried_object();
+
+$template = ( $category->slug != 'interviews' ) ? 'grid3' : 'interviews'
+
+?>
+<div class="container wrapper-margin">
+  <div class="row">
+    <div class='col-sm-12'>
+      <h1 class="text-center text-capitalize page-title">Tagged Under: <?php _e( $category->name );?></h1>
+      <div class="orbit-posts-wrapper">
+        <?php echo do_shortcode("[orbit_query posts_per_page='9' style='".$template."' cat='".$category->term_id."' pagination='1' ]"); ?>
+      </div>
+    </div>
+  </div>
+</div>
+<?php get_footer();?>
