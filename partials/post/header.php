f6084f99ab87b897212d4193aec2cfc6203e176b
psychome004 <anil.stephen66@gmail.com>
2022-10-27 11:57:43 +0530

    Update single post header

--- partials/post/header.php
+++ partials/post/header.php
@@ -11,8 +11,8 @@
     <div class="social-share"><?php echo do_shortcode('[addtoany]');?></div>
   </div>
 </div>
+<?php if( !empty( get_the_post_thumbnail() ) ): $image_url = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full' )[0]; ?>
 <div class="container sobt-col-width">
-  <?php if( !empty( get_the_post_thumbnail() ) ): $image_url = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full' )[0]; ?>
-    <div class="featured-img" style="background-image:url( <?php _e( $image_url );?> );" role="img" aria-label="<?php _e( get_the_title( $post->ID ) ); ?>"></div>
-  <?php endif;?>
+  <div class="featured-img" style="background-image:url( <?php _e( $image_url );?> );" role="img" aria-label="<?php _e( get_the_title( $post->ID ) ); ?>"></div>
 </div>
+<?php endif;?>
