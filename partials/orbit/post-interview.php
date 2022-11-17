d4e784842c9dbd74ff22a5f2c86e4f23597e959b
psychome004 <anil.stephen66@gmail.com>
2022-11-17 14:41:47 +0530

    Update orbit-interviews template

--- partials/orbit/post-interview.php
+++ partials/orbit/post-interview.php
@@ -2,7 +2,7 @@
 	$thumbnail = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full' )[0];
 	$permalink = get_the_permalink();
 	$icon_path = SOBT_THEME_URI."/assets/images/quote.svg";
-	$prefix 	 = '<span class="decoration" aria-hidden="true">&#8212;&#8212;</span>';
+	$prefix 	 = '<span class="decoration" aria-hidden="true">&#8212;</span>';
 ?>
 <div class="sobt-thumbnail-bg" style="background-image: url( <?php _e( $thumbnail );?> );">
   <a href="<?php _e( $permalink );?>" class="img-link">
